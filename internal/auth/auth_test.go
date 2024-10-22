ca4c6e9fe70eac7b5b8c8cfa27d8ff725f8d62db
Evan Deters <evanjd711@gmail.com>
2024-10-22 13:22:53 -0700

    fix test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -19,7 +19,7 @@ func TestGetAPIKey(t *testing.T) {
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
             got, err := GetAPIKey(tt.header)
-            if err == tt.wantErr {
+            if err != tt.wantErr {
                 t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
             }
             if got != tt.want {
